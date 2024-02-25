594f18274494cd1f16b3a9b20b8f28c6387f79a9
Moviesuploader <138097021+Moviesuploader@users.noreply.github.com>
2024-02-25 16:06:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -56,7 +56,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Verify
 VERIFY = bool(environ.get('VERIFY', True)) # Verification On ( True ) / Off ( False )
-HOWTOVERIFY = environ.get('HOWTOVERIFY', url='https://t.me/beautyofthemovie/3') # How to open tutorial link for verification
+HOWTOVERIFY = environ.get('HOWTOVERIFY', 'https://t.me/beautyofthemovie/3') # How to open tutorial link for verification
 
 # Others
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'bindaaslinks.com')
