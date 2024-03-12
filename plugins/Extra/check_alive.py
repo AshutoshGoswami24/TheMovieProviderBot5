80ed86e7b30c3ca0d73b7db4f5ab5d0c9f17d614
MarselDeveloper <141441864+marsel7official@users.noreply.github.com>
2024-03-12 12:07:44 +0530

    Add files via upload

---
+++ plugins/Extra/check_alive.py
@@ -0,0 +1,21 @@
+# Don't Remove Credit @movie_file_20
+# Subscribe YouTube Channel For Amazing Bot @movie_file_20
+# Ask Doubt on telegram @KingVJ01
+
+import time
+import random
+from pyrogram import Client, filters
+
+CMD = ["/", "."]
+
+@Client.on_message(filters.command("alive", CMD))
+async def check_alive(_, message):
+    await message.reply_text("𝖡𝗎𝖽𝖽𝗒 𝖨 𝖺𝗆 𝖠𝗅𝗂𝗏𝖾 :) 𝖧𝗂𝗍 /start.\n\n𝖧𝗂𝗍 /group_rule 𝖥𝗈𝗋 Group Rules\n\n𝖧𝗂𝗍 /ping 𝖳𝗈 𝖢𝗁𝖾𝖼𝗄 𝖡𝗈𝗍 𝖯𝗂𝗇𝗀 😉")
+
+@Client.on_message(filters.command("ping", CMD))
+async def ping(_, message):
+    start_t = time.time()
+    rm = await message.reply_text("...")
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    await rm.edit(f"🏓 ᴘɪɴɢ:\n{time_taken_s:.3f} ms")
