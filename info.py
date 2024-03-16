250507133eb9cf4ee0819a3072f18e695c569225
MarselDeveloper <141441864+marsel7official@users.noreply.github.com>
2024-03-16 19:42:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -106,8 +106,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://marselmovie-c39555b28a58.herokuapp.com/{}".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://marselmovie-c39555b28a58.herokuapp.com/{}".format(FQDN)
+URL = "https://moviebotx-ee96f200188f.herokuapp.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://moviebotx-ee96f200188f.herokuapp.com/".format(FQDN)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -122,9 +122,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',True))
 if HAS_SSL:
-    URL = "https://marselmovie-c39555b28a58.herokuapp.com/{}".format(FQDN)
+    URL = "https://moviebotx-ee96f200188f.herokuapp.com/".format(FQDN)
 else:
-    URL = "https://marselmovie-c39555b28a58.herokuapp.com/{}".format(FQDN)
+    URL = "https://moviebotx-ee96f200188f.herokuapp.com/".format(FQDN)
 
 
 # add premium logs channel id
